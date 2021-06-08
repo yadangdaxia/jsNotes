1ee332cb36f7d711543aa4f90bb06d3a7c313f35
yadangdaxia <matteson211@hotmail.com>
2021-06-08 14:48:13 +0800

    updated after finishing the lesson

--- notes.js
+++ notes.js
@@ -1,14 +1,66 @@
 
-// LOGICAL OR
+// SETTING VARIABLES AND IF/ELSE STATEMENTS
 
-var day = "Friday"; 
+var weather = "rainy";
+var sky = "sunny";
 
-if (day === "Saturday" || day === "Sunday") { 
-	console.log("It's the weekend!"); 
+if (weather ==="rainy", sky ==="sunny") {
+  console.log("It's rainy and sunny");
+}
+  else {
+    console.log("Weather is unknown.");
+  }
+
+
+
+// USING THE LOGICAL AND
+
+var rain = true;
+var wind = true;
+
+if (rain && wind) {
+  console.log("It's nasty weather out there.")
+}
+
+
+// USING THE LOGICAL OR
+
+var rain = true;
+var wind = false;
+
+if (rain || wind) {
+  console.log("It's nasty weather out there.")
+}
+
+
+
+
+// CONSOLE PRINTS OUT SOMETHING UNEXPECTED - Why do I get "Happy weekend." ? 
+
+var day = "Monday";
+
+if (day === "Saturday" || "Sunday") {
+  console.log("Happy weekend.")
 }
 else {
-  console.log("It's a weekday.")
+	console.log("It's the weekday.")
 }
+
+
+// CHECK CORRECT SYNTAX BELOW-//
+
+var day = "Monday";
+
+if (day === "Saturday" || day === "Sunday") {
+  console.log("Happy weekend.")
+}
+else {
+  console.log("It's the weekday.")
+}
+
+
+
+
 
 
 
@@ -62,17 +114,15 @@ console.log(bNames.trim())
 
 
 
-//  STARTS WITH "B", SIMPLIFIED
 
+//  STARTS WITH "B", SIMPLIFIED
 // PUT ALL VARIABLES INTO AN ARRAY, CREATE NEW VARIABLE
 
 var nombre = ["Ben", "Bob", "John", "Broadzilla" ];
-
 var bNames = " ";
 
 
 // RUN A FOR LOOP ON EACH ELEMENTS IN ARRAY, STARTING AT 0  
-
 
 for (var i = 0; i < nombre.length; i += 1) {
 	if (nombre[i][0] === "B") {
@@ -84,3 +134,38 @@ console.log(bNames.trim());
 
 // NOTE: SOMETIMES MY CODE DOESN'T SEEM TO FUNCTION, 
 // BUT WHEN REPASTING IT IN FROM SUBLIME IT IS OK (?)
+
+
+
+//FUNCTIONS (LIKE A BLACK BOX, PUT SOMETHING IN AND GET OUT A RETURN STATEMENT)
+//FUNCTION EXAMPLE: FUNCTION NAME IS fullName, ARGUMENTS ARE first AND last.
+//THIS FUNCTION CONCATENATES TWO STRINGS
+
+
+
+function fullName(first,last) {		//2. gets arguments
+	var nombre = first + " " + last;	//3. does  the concatenation
+	return nombre;	//4. returns the value
+}
+
+console.log(fullName("John", "Lennon")); //1. calls the function fullName / 5. outputs
+
+
+
+
+
+
+
+function firstCharacterFilter(element, character) {
+	var azNames = "";
+
+	for (var i = 0; i < element.length; i += 1) {
+		if (element[i][0] === character) {
+			azNames = element[i] + " " + azNames;
+	}
+}
+return azNames.trim();
+}
+
+var names = ["Bob", "John", "Gertrude", "Gary", "Bun-bun", "Steve", "Jackson", "Grapel"];
+console.log(firstCharacterFilter(names, "G"));
